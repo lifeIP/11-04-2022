e540e0afc6f6358ad7be5129b0828856852b3bcb
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-04-12 06:28:11 +0700

    constructor copy

--- 11,04,2022/Source.cpp
+++ 11,04,2022/Source.cpp
@@ -10,9 +10,10 @@ namespace ct {
 		colors color;
 		double weight=0;
 		char sex;
+		int sizeName;
 
 		void addMas(char* nameCat) {
-			int sizeName = strlen(nameCat);
+			sizeName = strlen(nameCat);
 			name = new char[sizeName+1];
 			name[sizeName] = 0;
 			for (int i = 0; i < strlen(nameCat); i++) {
@@ -20,17 +21,36 @@ namespace ct {
 			}
 		}
 	public:
+		// normal constructor
 		Cat(char* nameCat, ct::colors colorCat, char sex, double weight) {
 			addMas(nameCat);
 			color = colorCat;
 			this->weight = weight;
 			this->sex = sex;
 		}
+
+		// constructor copy
+		Cat(const Cat& other) {
+			this->color = other.color;
+			this->weight = other.weight;
+			this->sizeName = other.sizeName;
+			this->sex = other.sex;
+			this->name = new char[other.sizeName+1];
+			int i = 0;
+			for (; i < other.sizeName; i++) {
+				this->name[i] = other.name[i];
+			}
+			this->name[i] = 0;
+		}
+
 		void getDATAcat() {
 			std::cout << name << std::endl;
 			std::cout << color << std::endl;
 			std::cout << sex << std::endl;
 			std::cout << weight << std::endl;
+		}
+		void meows() {
+			std::cout << "meows" << std::endl;
 		}
 		void getThis() {
 			std::cout << this << std::endl;
@@ -44,29 +64,42 @@ namespace ct {
 	};
 }
 
+void func(ct::Cat copyValue) {
+	std::cout << "" << std::endl;
+	std::cout << "Cat4" << std::endl;
+	copyValue.getDATAcat();
+	copyValue.getThis();
+	copyValue.meows();
+}
+
 int main() {
 	std::cout << "Cats!!!" << std::endl;
-	char name1[] = "Murzik\0";
-	char name2[] = "Doggy\0";
-	char name3[] = "Dog\0";
+	char name1[] = { "Murzik\0" };
+	char name2[] = {"Doggy\0"};
+	char name3[] = { "Dog\0" };
 
 	ct::Cat cat1(name1, ct::Orange, 'M', 5);
 	ct::Cat cat2(name2, ct::Black, 'G', 156.25);
 	ct::Cat cat3(name3, ct::White, 'G', 21.565);
-	
+
 	std::cout << "" << std::endl;
 	std::cout << "Cat1" << std::endl;
 	cat1.getDATAcat();
 	cat1.getThis();
+	cat1.meows();
 
 	std::cout << "" << std::endl;
 	std::cout << "Cat2" << std::endl;
 	cat2.getDATAcat();
 	cat2.getThis();
+	cat2.meows();
 
 	//cat3.~Cat();
 	std::cout << "" << std::endl;
 	std::cout << "Cat3" << std::endl;
 	cat3.getDATAcat();
 	cat3.getThis();
+	cat3.meows();
+
+	func(cat1);
 }
